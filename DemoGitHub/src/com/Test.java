8a2555b30ec8b3889ba35fe7954d6cf027d6d71a
sumit <sumits3.123@gmail.com>
2023-01-29 16:58:26 +0530

    Student POJO Class Added.

--- DemoGitHub/src/com/Test.java
+++ DemoGitHub/src/com/Test.java
@@ -6,6 +6,11 @@ public class Test {
 
 		System.out.println("Check Brach-GItHub Creat Create Class");
 
-	}
+		Student stu = new Student();
+		stu.setId(12);
+		stu.setName("sumit");
+		stu.setAddress("pune");
 
+		System.out.println("Check Student Data :- " + stu);
+	}
 }
